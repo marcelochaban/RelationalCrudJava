1747d501cb044023548ac0f9fdb2b23ed155b232
Marcelo Chabán <marcelochaban@sirlib.com>
2022-11-17 17:19:04 -0300

    project creation
    In this version the project was created.
    Uploaded all the progress that was previously had in the "Practica" repository

---
+++ FinalProject/src/Clases/Conectar.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author MarceloWin
+ */
+public class Conectar {
+    
+    public static String user = "root";
+    public static String pass = "";
+     
+    Connection cn;
+    
+    public Connection conexion(){
+    
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd",user,pass);
+            System.out.println("Conectado");
+            
+        } catch (Exception e) {
+            System.out.println("No se pudo realizar la conexión");
+            System.err.println(e.getMessage());
+
+        }
+        return cn;
+    }
+    
+    
+}
